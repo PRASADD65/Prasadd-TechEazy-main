ffb30324b0902546a90d64eca596ed58c306d6a7
Durga Prasad <durgaaprasadd765@gmail.com>
2025-07-13 01:06:54 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,15 +2,18 @@ data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_subnet_ids" "default" {
-  vpc_id = data.aws_vpc.default.id
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
 
 resource "aws_instance" "github_runner" {
   ami                         = var.ami_id
   instance_type               = var.instance_type
   key_name                    = var.key_name
-  subnet_id                   = data.aws_subnet_ids.default.ids[0]
+  subnet_id                   = data.aws_subnets.default.ids[0]
   vpc_security_group_ids      = [aws_security_group.github_runner.id]
   iam_instance_profile        = var.iam_instance_profile
 
