96863defe153d3894f0faf29802d2586836dbae5
Durga Prasad <durgaaprasadd765@gmail.com>
2025-07-12 22:22:46 +0530

    Update and rename main.tf to ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,13 @@
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
 
 resource "aws_instance" "github_runner" {
   ami                         = var.ami_id
   instance_type               = var.instance_type
   key_name                    = var.key_name
-  subnet_id                   = var.subnet_id
-  vpc_security_group_ids      = var.vpc_security_group_ids
+  subnet_id                   = data.aws_subnet_ids.default.ids[0]
+  vpc_security_group_ids      = [aws_security_group.github_runner.id]
   iam_instance_profile        = var.iam_instance_profile
 
   associate_public_ip_address = true
@@ -16,4 +19,7 @@ resource "aws_instance" "github_runner" {
   tags = {
     Name = "GitHubRunnerEC2"
   }
+
+  # If you ever have dependencies (like IAM resources), add them here:
+  # depends_on = [aws_iam_instance_profile.example]
 }
