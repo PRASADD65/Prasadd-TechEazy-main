d493e91877da94a83c3eb0858f4a74c289bad9b0
Durga Prasad <durgaaprasadd765@gmail.com>
2025-07-13 10:26:30 +0530

    Update ec2config.sh

--- ec2config.sh
+++ ec2config.sh
@@ -19,7 +19,7 @@ RUNNER_VERSION="${RUNNER_VERSION}"
 curl -o actions-runner-linux-x64.tar.gz -L https://github.com/actions/runner/releases/download/v${RUNNER_VERSION}/actions-runner-linux-x64-${RUNNER_VERSION}.tar.gz
 tar xzf actions-runner-linux-x64.tar.gz
 
-GH_REPO_URL="https://github.com/PRASADD65/tech_eazy_PRASADD65_aws_internship"
+GH_REPO_URL="${GH_REPO_URL}"
 GH_RUNNER_TOKEN="${github_runner_token}"
 ./config.sh --url ${GH_REPO_URL} --token ${GH_RUNNER_TOKEN} --unattended --name root-runner --labels self-hosted,ubuntu,ec2
 ./run.sh &
