aced6154b5e0e7781cb6b9d889b9dbadd21ae465
Durga Prasad <durgaaprasadd765@gmail.com>
2025-07-20 01:18:25 +0530

    Update ec2config.sh

--- ec2config.sh
+++ ec2config.sh
@@ -379,5 +379,7 @@ chmod +x /root/log_parser.py
 echo "✅ Log Parser Written" >> /var/log/cloud-init-output.log
 
 # Register cron job to run every 5 minutes
-(sudo crontab -l 2>/dev/null; echo "*/5 * * * * /usr/bin/python3 /root/log_parser.py") | sudo crontab -
+echo "*/5 * * * * /usr/bin/python3 /root/log_parser.py" | sudo crontab -
+sudo crontab -l > /var/log/cron_status.txt
 echo "✅ Cron job registered" >> /var/log/cloud-init-output.log
+
